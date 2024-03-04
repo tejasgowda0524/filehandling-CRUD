0d98ebd59c8e1e12d5c4fe44ea65aac85575b4e3
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 23:04:03 +0530

    ..

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Security.Cryptography.X509Certificates;
+namespace filehandling1
+{
+
+
+    class addressbook
+    {
+        string path = "F:\\newFile.cs";
+
+        public void addcontacts(string a, string b, string c)
+        {
+            string line = $"{a},{b},{c}";
+
+            File.WriteAllText(path, line);
+            //using (StreamWriter writer = File.AppendText(path))
+            //{
+            //    writer.WriteLine(line);
+            //}
+            Console.WriteLine("added successfully");
+
+
+            Console.ReadLine();
+
+        }
+
+        public void Editcontact(string m1, string m2)
+        {
+            string[] lines = File.ReadAllLines(path);
+            bool edited = false;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] s = lines[i].Split(',');
+                if (s.Length >= 3 && s[0].Equals(m1) && s[1].Equals(m2))
+                {
+                    Console.WriteLine("Enter new age:");
+                    string newAge = Console.ReadLine();
+
+
+                    s[2] = newAge;
+
+
+                    lines[i] = string.Join(",", s);
+
+                    edited = true;
+                    Console.WriteLine("Edited successfully");
+                    break;
+                }
+            }
+
+            if (edited)
+            {
+
+                File.WriteAllLines(path, lines);
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+
+        public void deletecontact(string m1, string m2)
+        {
+            string[] lines = File.ReadAllLines(path);
+            bool deleted = false;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] s = lines[i].Split(',');
+                if (s.Length >= 2 && s[0].Equals(m1) && s[1].Equals(m2))
+                {
+                    Console.WriteLine("Deleting contact: " + lines[i]);
+                    lines[i] = null;
+                    deleted = true;
+                    Console.WriteLine("Deleted successfully");
+                    break;
+                }
+            }
+
+            if (deleted)
+            {
+
+                File.WriteAllLines(path, lines.Where(line => line != null));
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+
+        public void displaycontact()
+        {
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            addressbook ab = new addressbook();
+
+            while (true)
+            {
+                Console.WriteLine("choose the option\n1: add contacts\n2: edit contacts\n3: delete contact\n4: display contact\n5. exit");
+                int im = int.Parse(Console.ReadLine());
+
+                switch (im)
+                {
+                    case 1:
+                        Console.WriteLine("enter your name");
+                        String name = Console.ReadLine();
+                        Console.WriteLine("enter your last  name");
+                        String L_name = Console.ReadLine();
+                        Console.WriteLine("enter your age");
+                        String age = Console.ReadLine();
+
+                        ab.addcontacts(name, L_name, age);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("enter first name to edit");
+                        String name1 = Console.ReadLine();
+                        Console.WriteLine("enter last name to edit");
+                        String name2 = Console.ReadLine();
+
+                        ab.Editcontact(name1, name2);
+                        break;
+                    case 3:
+                        Console.WriteLine("enter first name to delete");
+                        String nam1 = Console.ReadLine();
+                        Console.WriteLine("enter last name to delete");
+                        String nam2 = Console.ReadLine();
+
+                        ab.deletecontact(nam1, nam2);
+                        break;
+                    case 4:
+                        ab.displaycontact();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+
+}
+
---
+++ prac.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace filehandling1
+//{
+//    /*class Employee
+//    {
+//        public string Name {  set; get; }
+//        public string Department { get; set; }
+//        public int JoiningDate { get; set; }
+//    }*/
+//    public class prac
+//    {
+//        static void Main(string[] args)
+//        {
+//            /*List<Employee> emp = new List<Employee>
+//            {
+//                new Employee { Name = "tejas", Department = "ece", JoiningDate = 3 },
+//                new Employee { Name = "rahul", Department = "cse", JoiningDate = 8 },
+//                new Employee { Name = "john", Department = "is", JoiningDate = 6 },
+//                new Employee { Name = "manu", Department = "mech", JoiningDate = 2 }
+
+//            };
+
+//            //var res = from x in emp where x.JoiningDate<6  select x;
+
+//            var res = emp.OrderByDescending(x => x.JoiningDate);
+
+//            foreach(var x in res)
+//            {
+//                Console.WriteLine($"employee: {x.Name} ,department {x.Department} , joining date {x.JoiningDate}");*/
+
+
+
+
+          
+//        }
+
+//    }
+
+    
+//}
